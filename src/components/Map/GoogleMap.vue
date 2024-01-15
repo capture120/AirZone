484cf66a6c7692a862096f8300f6bd23705ac14d
Nathan Jung <NJUNG@MITRE.ORG>
2024-01-15 17:38:23 -0500

    add marker with AdvancedMarker api

--- src/components/Map/GoogleMap.vue
+++ src/components/Map/GoogleMap.vue
@@ -7,11 +7,11 @@ const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 const GOOGLE_MAP_ID = import.meta.env.VITE_GOOGLE_MAP_ID;
 
 const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
-const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 const position = { lat: -25.344, lng: 131.031 };
 
 onMounted(async () => {
   const { Map } = await loader.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
   const map = new Map(
     document.getElementById('map') as HTMLElement,
     {
@@ -21,7 +21,7 @@ onMounted(async () => {
     }
   );
 
-  const marker = new AdvancedMarkerElement({
+  new AdvancedMarkerElement({
     map: map,
     position: position,
     title: 'The MITRE Corporation'
