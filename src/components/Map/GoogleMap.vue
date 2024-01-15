f52c919e28048525fc92333432cad4f78b6ad8b2
Nathan Jung <NJUNG@MITRE.ORG>
2024-01-15 17:34:23 -0500

    failed integration of custom web component library

--- src/components/Map/GoogleMap.vue
+++ src/components/Map/GoogleMap.vue
@@ -1,41 +1,55 @@
 <script setup lang="ts">
-  import { Loader } from "@googlemaps/js-api-loader"
-  let map: google.maps.Map;
-  async function initMap(): Promise<void> {
-    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-    map = new Map(document.getElementById("map") as HTMLElement, {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-  }
+// import '@googlemaps/extended-component-library/overlay_layout.js';
+// import '@googlemaps/extended-component-library/api_loader.js';
 
-  initMap();
-  const loader = new Loader({
-    apiKey: "AIzaSyAbcRcBKY1Q5knGDie9IVfviuhXRI1zAaI",
-    version: "weekly",
-  });
+import { Loader } from '@googlemaps/js-api-loader'
+import { onMounted } from 'vue';
 
-  loader.load().then(async () => {
-    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-    map = new Map(document.getElementById("map") as HTMLElement, {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+const GOOGLE_MAP_ID = import.meta.env.VITE_GOOGLE_MAP_ID;
+
+const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY })
+const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
+const position = { lat: -25.344, lng: 131.031 };
+
+onMounted(async () => {
+  const { Map } = await loader.importLibrary("maps");
+  const map = new Map(
+    document.getElementById('map') as HTMLElement,
+    {
+      zoom: 15,
+      center: position,
+      mapId: GOOGLE_MAP_ID,
+    }
+  );
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: 'The MITRE Corporation'
   });
+});
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">Hello World</h1>
-    <gmp-map center="42.50435256958008,-71.23621368408203" zoom="14" map-id="DEMO_MAP_ID">
-      <gmp-advanced-marker position="42.50435256958008,-71.23621368408203" title="My location">
-      </gmp-advanced-marker>
+  <!-- <gmpx-api-loader key="AIzaSyAbcRcBKY1Q5knGDie9IVfviuhXRI1zAaI"/>
+  <gmpx-split-layout>
+    <gmpx-place-overview slot="fixed" place="ChIJ39Y-tdg1fYcRQcZcBb499do"></gmpx-place-overview>
+    <gmp-map slot="main" center="43.880,-103.459" zoom="10" map-id="eed0a5d7278df36b">
+      <gmp-advanced-marker position="43.880,-103.459"></gmp-advanced-marker>
     </gmp-map>
+  </gmpx-split-layout> -->
+  <div>
+    <div class="google-map" id="map" ></div>
   </div>
 </template>
 
+
 <style scoped>
-  gmp-map {
-    height: 100%;
-  }
+
+.google-map {
+  width: 100%;
+  height:80vh;
+}
+
 </style>
